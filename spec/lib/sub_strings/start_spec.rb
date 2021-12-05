3347b17f0c4aa61c2dcb4e636504749fea8dd632
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2021-12-05 12:50:58 -0300

    discarding all collection membership tests

--- spec/lib/sub_strings/start_spec.rb
+++ spec/lib/sub_strings/start_spec.rb
@@ -30,10 +30,6 @@ RSpec.describe Start do
 
       it { is_expected.to include('i' => 3) }
       it { is_expected.to eq main.result }
-      it {
-        is_expected.to have_attributes({ 'down' => 1, 'go' => 1, 'going' => 1, 'how' => 2, 'howdy' => 1, 'it' => 2,
-                                         'i' => 3, 'own' => 1, 'part' => 1, 'partner' => 1, 'sit' => 1 })
-      }
     end
   end
 end
